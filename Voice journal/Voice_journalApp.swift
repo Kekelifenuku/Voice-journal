2e4e9237cb6e53a1d4a090d1b750b398462da4a9
fenuku kekeli <fenuku.kekeli8989@gmail.com>
2026-03-11 00:54:53 +0000

    Initial Commit

---
+++ Voice journal/Voice_journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_journalApp.swift
+//  Voice journal
+//
+//  Created by Fenuku kekeli on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Voice_journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
